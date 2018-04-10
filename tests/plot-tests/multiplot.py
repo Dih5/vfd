b47c859d2ec165643c39f831db95634624940762
Dih5 <guillehg@gmail.com>
2018-04-10 23:05:18 +0200

    Fix test

--- tests/plot-tests/multiplot.py
+++ tests/plot-tests/multiplot.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
 f, axarr = plt.subplots(2, 2)
-axarr[0][0].plot(*[[0, 1, 2, 3], [0, 1, 2, 3]])
+axarr[0][0].plot([0, 1, 2, 3], [0, 1, 2, 3])
 axarr[0][0].set_xlabel(r"Plot 0,0")
-axarr[0][1].plot(*[[0, 1, 4, 9], [0, 1, 2, 3]])
+axarr[0][1].plot([0, 1, 4, 9], [0, 1, 2, 3])
 axarr[0][1].set_xlabel(r"Plot 1,0")
-axarr[1][0].plot(*[[0, 1, 2, 3], [0, 1, 4, 9]])
+axarr[1][0].plot([0, 1, 2, 3], [0, 1, 4, 9])
 axarr[1][0].set_xlabel(r"Plot 0,1")
-axarr[1][1].plot(*[[0, 1, 4, 9], [0, 1, 4, 9]])
+axarr[1][1].plot([0, 1, 4, 9], [0, 1, 4, 9])
 axarr[1][1].set_xlabel(r"Plot 1,1")
-plt.gcf().canvas.set_window_title(r"multiplot")
-plt.show()
+plt.savefig("multiplot.png")
