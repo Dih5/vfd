29065846b2c0f2c3194306bff414d20a10f22201
Dih5 <guillehg@gmail.com>
2018-04-11 01:31:07 +0200

    Use multiple styles from CLI
    Parameter renamed to style to match matplotlib nomenclature.

--- vfd/cli.py
+++ vfd/cli.py
@@ -10,12 +10,16 @@ from . import vfd
 @click.command()
 @click.argument('file', nargs=-1)
 @click.option('--format', default='', help='Format of the output files. If none, an interactive window will open.')
-@click.option('--theme', default='', help='Theme to use to generate the plot')
-def main(file, format, theme):
-    """Console script for vfd."""
+@click.option('--style', default='', help='Matplotlib style(s) to use to generate the plot. Styles can be combined '
+                                          'with a / (no spaces among them). Styles further to the right overwrite '
+                                          'values defined by styles to their left.')
+def main(file, format, style):
+    """Command line interface for Vernacular Figure Description."""
+    if style is not None and "/" in style:
+        style = style.split("/")
     if file:
         for f in file:
-            vfd.create_scripts(path=f, export_format=format, context=theme, run=True)
+            vfd.create_scripts(path=f, export_format=format, context=style, run=True)
     else:
         _print_help_msg(main)
     return 0
