e0c2f6c855d0c8712645ad50459c4fbb6c41a6fa
guillehg <guillehg@gmail.com>
2018-04-11 11:37:11 +0200

    Change style separator to comma

--- vfd/cli.py
+++ vfd/cli.py
@@ -11,12 +11,14 @@ from . import vfd
 @click.argument('file', nargs=-1)
 @click.option('--format', default='', help='Format of the output files. If none, an interactive window will open.')
 @click.option('--style', default='', help='Matplotlib style(s) to use to generate the plot. Styles can be combined '
-                                          'with a / (no spaces among them). Styles further to the right overwrite '
+                                          'with commas (no spaces among them). Styles further to the right overwrite '
                                           'values defined by styles to their left.')
 def main(file, format, style):
     """Command line interface for Vernacular Figure Description."""
-    if style is not None and "/" in style:
-        style = style.split("/")
+    # NOTE: Styles with a comma in their names (!) won't be processed properly.
+    # If we wanted to support this, a escape procedure should be defined.
+    if style is not None and "," in style:
+        style = style.split(",")
     if file:
         for f in file:
             vfd.create_scripts(path=f, export_format=format, context=style, run=True)
