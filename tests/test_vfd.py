b28186ac8b390574ddf4fe02890fe6718afc717a
Dih5 <dihedralfive@gmail.com>
2018-04-11 22:19:22 +0200

    Parametrize plot tests

--- tests/test_vfd.py
+++ tests/test_vfd.py
@@ -10,6 +10,7 @@ import filecmp
 from glob import glob
 
 from click.testing import CliRunner
+import pytest
 
 from vfd import cli
 from vfd import vfd
@@ -35,22 +36,27 @@ def test_errobar():
     assert vfd._full_errorbar([1, 2, 3], [2, 3, 4], 2, True) == [1, 1, 1]
 
 
-def test_plot_files(tmpdir):
+@pytest.fixture
+def get_plot_test_list():
+    return glob(os.path.join("tests", "plot-tests", "*.vfd"))
+
+
+@pytest.mark.parametrize('file', get_plot_test_list())
+def test_plot_files(tmpdir, file):
     """Test the plotting is working"""
     export_format = 'png'
 
-    for file in glob(os.path.join("tests", "plot-tests", "*.vfd")):
-        # Copy the .vfd
-        name = os.path.basename(file)
-        temp_vfd = os.path.join(tmpdir, name[:-3] + "test.vfd")
-        shutil.copyfile(file, temp_vfd)
-        # Run it
-        vfd.create_scripts(temp_vfd, run=True, blocking=True, export_format=export_format)
-        # Copy the reference script
-        temp_ref = os.path.join(tmpdir, name[:-3] + "py")
-        shutil.copyfile(file[:-3] + "py", temp_ref)
-        # Run it
-        proc = subprocess.Popen(["python", os.path.abspath(temp_ref)], cwd=os.path.abspath(tmpdir))
-        proc.wait()
-        # Compare the files
-        assert filecmp.cmp(temp_vfd[:-3] + export_format, temp_ref[:-2] + export_format)
+    # Copy the .vfd
+    name = os.path.basename(file)
+    temp_vfd = os.path.join(tmpdir, name[:-3] + "test.vfd")
+    shutil.copyfile(file, temp_vfd)
+    # Run it
+    vfd.create_scripts(temp_vfd, run=True, blocking=True, export_format=export_format)
+    # Copy the reference script
+    temp_ref = os.path.join(tmpdir, name[:-3] + "py")
+    shutil.copyfile(file[:-3] + "py", temp_ref)
+    # Run it
+    proc = subprocess.Popen(["python", os.path.abspath(temp_ref)], cwd=os.path.abspath(tmpdir))
+    proc.wait()
+    # Compare the files
+    assert filecmp.cmp(temp_vfd[:-3] + export_format, temp_ref[:-2] + export_format)
