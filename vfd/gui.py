671ea03b353d82160f9c242791ca25b1c4b323a3
guillehg <guillehg@gmail.com>
2018-06-29 10:52:33 +0200

    Improve prototype

--- vfd/gui.py
+++ vfd/gui.py
@@ -5,8 +5,8 @@
 import sys
 import os
 
-from . import vfd
-from . import __version__
+import shutil
+from PIL import Image, ImageTk
 
 try:
     import tkinter as tk
@@ -16,54 +16,69 @@ except ImportError:
     import Tkinter as tk
     import Tkinter.filedialog as tkfiledialog
 
-from PIL import Image, ImageTk
+from . import vfd
+from . import __version__
 
 img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "img")
 
 
-class VfdGui(tk.Frame):
+class VfdGui(tk.Frame, object):
 
     def __init__(self, master=None):
-        super().__init__(master=master)
+        super(VfdGui, self).__init__(master=master)
 
         self.master.title("".join(('VFD GUI v', __version__)))
 
-        menu = tk.Menu(self.master)
+        self.menu = tk.Menu(self.master)
         self.file_menu = tk.Menu(self.master, tearoff=0)
         self.file_menu.add_command(label="Open", command=self.open_choose)
+        self.file_menu.add_command(label="Export as xlsx", command=self.export_xlsx_choose)
         self.file_menu.add_command(label="Exit", command=self.leave)
-        menu.add_cascade(label="File", menu=self.file_menu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
 
-        toolbar = tk.Frame(self.master, bd=1, relief=tk.RAISED)
+        self.toolbar = tk.Frame(self, bd=1, relief=tk.RAISED)
 
         img_open = ImageTk.PhotoImage(Image.open(os.path.join(img_path, "document-open.png")))
-        btn_open = tk.Button(toolbar, image=img_open, relief=tk.FLAT, command=self.open_choose)
+        btn_open = tk.Button(self.toolbar, image=img_open, relief=tk.FLAT, command=self.open_choose)
         btn_open.image = img_open
         btn_open.pack(side=tk.LEFT, padx=2, pady=2)
 
         img_xlsx = ImageTk.PhotoImage(Image.open(os.path.join(img_path, "x-office-spreadsheet.png")))
-        btn_xlsx = tk.Button(toolbar, image=img_xlsx, relief=tk.FLAT, command=self.export_xlsx)
+        btn_xlsx = tk.Button(self.toolbar, image=img_xlsx, relief=tk.FLAT, command=self.export_xlsx_choose)
         btn_xlsx.image = img_xlsx
         btn_xlsx.pack(side=tk.LEFT, padx=2, pady=2)
 
         img_exit = ImageTk.PhotoImage(Image.open(os.path.join(img_path, "system-log-out.png")))
-        btn_exit = tk.Button(toolbar, image=img_exit, relief=tk.FLAT, command=self.quit)
+        btn_exit = tk.Button(self.toolbar, image=img_exit, relief=tk.FLAT, command=self.quit)
         btn_exit.image = img_exit
         btn_exit.pack(side=tk.LEFT, padx=2, pady=2)
 
-        toolbar.pack(side=tk.TOP, fill=tk.X)
-        self.master.config(menu=menu)
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
+        self.master.config(menu=self.menu)
 
-        self.preview = tk.Label(master, text="Preview will be shown here")
-        self.preview.pack(side=tk.TOP)
+        self.preview_frame = tk.Frame(self)
+
+        self.preview_toolbar = tk.Frame(self.preview_frame, bd=1, relief=tk.RAISED)
+
+        self.preview_refresh = tk.Button(self.preview_toolbar, image=img_xlsx, relief=tk.FLAT,
+                                         command=self.export_xlsx_choose)
+        self.preview_refresh.pack(side=tk.RIGHT)
+
+        self.preview_toolbar.pack(side=tk.TOP)
+
+        self.preview = tk.Label(self.preview_frame, text="Preview will be shown here")
+        self.preview.pack(side=tk.BOTTOM)
+
+        self.preview_frame.pack(side=tk.RIGHT)
+
         self.pack()
 
         self.file_path = None
 
     def open_choose(self):
-        ifile = tkfiledialog.askopenfile(parent=self, mode='r', title='Open a VFD')
-        path = ifile.name
-        self.open(path)
+        file = tkfiledialog.askopenfile(parent=self, mode='r', filetypes=(("VFD file", "*.vfd"), ("all files", "*.*")),
+                                        title='Open a VFD')
+        self.open(file.name)
 
     def open(self, path):
         self.file_path = path
@@ -76,8 +91,17 @@ class VfdGui(tk.Frame):
         self.preview.image = img
         pass
 
-    def export_xlsx(self):
+    def export_xlsx_choose(self):
+        file = tkfiledialog.asksaveasfilename(parent=self, filetypes=(("Spreadsheet", "*.xlsx"),),
+                                              title='Export as xlsx')
+        self.export_xlsx(file)
+
+    def export_xlsx(self, path=None):
         vfd.create_xlsx(self.file_path)
+        gen_path = self.file_path[:-3] + "xlsx"
+        # TODO: Check if exists
+        if path is not None and path != gen_path:
+            shutil.move(gen_path, path)
 
     def leave(self):
         self.quit()
